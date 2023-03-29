221cd91f316c2225f3ca9d127559a903c5721d95
USER <USER@DESKTOP-C086S32>
2023-03-29 22:23:20 +0430

    Add project files.

---
+++ Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetAll();
+        Task<Category> GetById(int id);
+        Task<bool> Create (Category category);
+        Task<bool> Update (Category category);
+        Task<bool> Delete (int id);
+    }
+}
---
+++ Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAll();
+        Task<Product> GetById(int id);
+        Task<bool> Create(Product product);
+        Task<bool> Update(Product product);
+        Task<bool> Delete(int id);
+
+    }
+}
