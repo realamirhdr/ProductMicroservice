38380136bbe6ec508acdae83209ad0c3b82b6b15
USER <USER@DESKTOP-C086S32>
2023-03-30 07:40:22 +0430

    - added Dtos and Get actions

---
+++ Application/Category/Query/GetAllCategories/GetAllCategoriesQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Category.Dto;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Category.Query.GetAllCategories
+{
+    public class GetAllCategoriesQuery : IRequest<List<GetCategoryDto>>
+    {
+    }
+}
--- Product/Program.cs
+++ Product/Program.cs
@@ -1,10 +1,15 @@
 using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using System.Configuration;
+using Domain.Interfaces;
+using Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
